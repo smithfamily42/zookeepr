1c70d68bbc3e7e6689d5c3d53acdef9ed1c8479d
Brad Smith <smithfamily42@gmail.com>
2021-05-30 22:00:45 -0700

    final changes to zookeeper

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
+
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
 
